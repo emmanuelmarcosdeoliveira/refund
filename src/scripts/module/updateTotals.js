dda6d48d20e0c11e0e4f757f5c1a6d6af6321c4d
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2025-02-14 22:13:35 -0300

    feat:  Functionality that adds up expenses has been developed

--- src/scripts/module/updateTotals.js
+++ src/scripts/module/updateTotals.js
@@ -1,15 +1,48 @@
 export function UpdateTotal() {
   const expenseQuantity = document.querySelector("aside header p span");
   const expenseList = document.querySelector("ul");
-  console.log(expenseList);
+  const expensesTotal = document.querySelector("aside header h2");
+
+  function formatCurrencyBRL(value) {
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+    // Abaixo estamos retornando o valor formatado de acordo com nossa moeda
+    return value;
+  }
   try {
-    const itens = expenseList.children;
-    expenseQuantity.textContent = `${itens.length} ${
-      itens.length > 1 ? "despesas" : "despesa"
+    const items = expenseList.children;
+    expenseQuantity.textContent = `${items.length} ${
+      items.length > 1 ? "despesas" : "despesa"
     }`;
-    console.log(itens);
+    // Variável para incrementar o total
+    let total = 0;
+    // Percorre os itens da list
+    for (let item = 0; item < items.length; item++) {
+      const itemAmount = items[item].querySelector(".expense-amount");
+      let value = itemAmount.textContent
+        .replace(/[^\d,]/g, "")
+        .replace(",", ".");
+      // convert o valor para float
+      value = parseFloat(value);
+      // Verificar se é um numero valido
+      if (isNaN(value)) {
+        return alert(
+          "Não foi possível fazer o calculo do total o valor não parece um  número"
+        );
+      }
+      // Incrementa o valor total
+      total += Number(value);
+    }
+    // Adiciona a span
+    const symbolBRL = document.createElement("small");
+    symbolBRL.textContent = "R$";
+
+    total = formatCurrencyBRL(total).toUpperCase().replace("R$", "");
+    expensesTotal.innerHTML = "";
+    expensesTotal.append(symbolBRL, total);
   } catch (error) {
-    console.log(error);
-    alert("Erro");
+    alert("Erro ao calcular o total");
   }
 }
